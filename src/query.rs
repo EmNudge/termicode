f55a7600b223c62b993ec22bd854cc7774174177
EmNudge <24513691+EmNudge@users.noreply.github.com>
2022-05-24 00:06:37 -0400

    fix trying next on empty iter

--- src/query.rs
+++ src/query.rs
@@ -1,11 +1,13 @@
 use crate::data::{UnicodeData, UnicodeFile};
 use regex::Regex;
 
+const MAX_GAP: (u32, u32) = (u32::MAX, u32::MAX);
+
 fn get_unicode_items<'a>(
     unicode_file: &'a UnicodeFile,
 ) -> Box<dyn Iterator<Item = &'a UnicodeData> + 'a> {
     let mut gaps_iter = unicode_file.gaps.clone().into_iter();
-    let mut next_gap = gaps_iter.next().unwrap_or((u32::MAX, u32::MAX));
+    let mut next_gap = gaps_iter.next().unwrap_or(MAX_GAP);
 
     let mut hash_index: u32 = 0;
 
@@ -13,7 +15,7 @@ fn get_unicode_items<'a>(
         hash_index += 1;
         if hash_index > next_gap.0 {
             hash_index = next_gap.1;
-            next_gap = gaps_iter.next().unwrap()
+            next_gap = gaps_iter.next().unwrap_or(MAX_GAP);
         }
 
         unicode_file.map.get(&hash_index).unwrap()
